3ea29f128c1630fc3d3909cc8fab5e40368f7b69
IconicDexter <kennymwendwa67@gmail.com>
2022-12-22 11:51:48 +0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import Header from "../components/Header"
+import HeroHome from "../components/HeroHome"
+import FeaturesHome from "../components/Features"
+import FeaturesBlocks from "../components/FeaturesBlocks"
+import Testimonials from "../components/Testimonials"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+        {/*  Page sections */}
+        <HeroHome />
+        <FeaturesHome />
+        <FeaturesBlocks />
+        <Testimonials />
+        <Newsletter />
+      </main>
+
+      {/*  Site footer */}
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
